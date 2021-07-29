4731e7cc74cbb69357b4b5564d838941355b34ca
Steve VM <steve.j.cevallos@gmail.com>
2021-07-29 19:03:47 +0000

    Send

---
+++ BMS/bms.c
@@ -0,0 +1,17 @@
+#include < "bms.h" >
+
+/*
+see()
+erase()
+transact()
+edit()
+view_list()
+new_acc()
+*/
+
+void menu()
+
+void main()
+{
+
+}
---
+++ BMS/bms.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _customer
+{
+    char * name;
+    int month;
+    int day;
+    int year;
+    int citizen_num;
+    char * address;
+    int phone_number;
+} customer;
