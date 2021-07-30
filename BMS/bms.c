3b94a85f01f78efd11746e1de7550a2930af6146
Steve VM <steve.j.cevallos@gmail.com>
2021-07-30 13:53:00 +0000

    Added the Menu

--- BMS/bms.c
+++ BMS/bms.c
@@ -1,4 +1,4 @@
-#include < "bms.h" >
+#include "bms.h"
 
 /*
 see()
@@ -6,12 +6,75 @@ erase()
 transact()
 edit()
 view_list()
-new_acc()
+
 */
 
-void menu()
-
-void main()
+void new_acc()
 {
+    puts("Create New Account");
 
+
+}
+
+void menu(void)
+{
+    int user_input;
+
+    //Display the Welcome Screen
+    puts("\n\t\t CUSTOMER ACCOUNT BANKING MANAGMENT SYSTEM \n");
+    puts("\t\t WELCOME TO THE MAIN MENU \n");
+    puts("\t 1. Create New Account");
+    puts("\t 2. Update information of exisiting account");
+    puts("\t 3. For transactions");
+    puts("\t 4. Check the details fo existing account");
+    puts("\t 5. Remove existing account");
+    puts("\t 6. View customer's list");
+    puts("\t 7. Exit \n");
+
+    printf("\t Enter your choice: ");
+    scanf("%d", &user_input);
+
+    //Checks to see if the input is valid
+    switch (user_input)
+    {
+    case 1:
+        new_acc();
+        break;
+    case 2:
+        puts("Selected Update Info");
+        break;
+
+    case 3:
+        puts("Selected Transactions");
+        break;
+
+    case 4:
+        puts("Selected Check Details");
+        break;
+
+    case 5:
+        puts("Selected Remove Account");
+        break;
+
+    case 6:
+        puts("Selected Customer List");
+        break;
+
+    case 7:
+        puts("Selected Exit");
+        puts("Goodbye!");
+        exit(EXIT_SUCCESS);
+        break;
+
+    default:
+        puts("Invalid Input Goodbye");
+        exit(EXIT_FAILURE);
+        break;
+    }
+}
+
+void main(void)
+{
+    // Runs the menu function
+    menu();
 }
--- BMS/bms.h
+++ BMS/bms.h
@@ -1,13 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+
+
+typedef struct _account
+{
+    int account_number;
+    char * account_type;
+    int balance;
+} account;
+
 
 typedef struct _customer
 {
     char * name;
-    int month;
-    int day;
-    int year;
-    int citizen_num;
+    int dob_month;
+    int dob_day;
+    int dob_year;
+    double citizen_num;
     char * address;
     int phone_number;
+    account customer_account;
 } customer;
